89a9f75255150e20e4446eaef8a9ea067758dc06
Larissa Ribeiro de Souza <souzalarissa383@gmail.com>
2025-02-04 13:23:13 -0300

    add comments

--- lib/core/usecases/get_filmes_populares.dart
+++ lib/core/usecases/get_filmes_populares.dart
@@ -1,12 +1,17 @@
+// Importa o modelo de filme
 import 'package:movie_app_flutter/feature/data/models/movie.dart';
+// Importa a implementação do repositório de filmes
 import 'package:movie_app_flutter/feature/data/repository/movie_repository_impl.dart';
 
-
+// Classe responsável por obter a lista de filmes populares
 class GetFilmesPopulares {
+  // Repositório de filmes que será utilizado para buscar os dados
   final FilmeRepositoryImpl repository;
-
+  
+  // Construtor que recebe o repositório como parâmetro
   GetFilmesPopulares(this.repository);
-
+  
+  // Método assíncrono que retorna a lista de filmes populares
   Future<List<Movie>> call() async {
     return await repository.buscarFilmesPopulares();
   }
